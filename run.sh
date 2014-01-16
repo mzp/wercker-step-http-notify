e0f8c09bff0516b831cd5ea4c09c5f499e205fda
mzp <mzpppp@gmail.com>
2014-01-16 18:09:20 +0400

    send web hook with some parameters

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-curl -d 'x=x' $WERCKER_HTTP_NOTIFY_URL
+
+if [ ! -n "$DEPLOY" ]; then
+  export WERCKER_HTTP_NOTIFY_TYPE="build"
+else
+  export WERCKER_HTTP_NOTIFY_TYPE="deploy"
+fi
+
+curl -d "result=$WERCKER_RESULT" \
+     -d "type=$WERCKER_HTTP_NOTIFY_TYPE" \
+     -d "application_owner_name=$WERCKER_APPLICATION_OWNER_NAME" \
+     -d "application_name=$WERCKER_APPLICATION_NAME" \
+     -d "git_branch=$WERCKER_GIT_BRANCH" \
+     -d "started_by=$WERCKER_STARTED_BY" \
+     $WERCKER_HTTP_NOTIFY_URL
